01284de1423be68f2c011a53a10a750485d2312b
Mohd Taiyyab <taiyyabmohammad11@gmail.com>
2023-11-29 00:15:46 +0530

    Developed Basic Architecture

---
+++ src/main/java/com/fifajwt/amigosjwt/user/Role.java
@@ -0,0 +1,6 @@
+package com.fifajwt.amigosjwt.user;
+
+public enum Role {
+    USER,
+    ADMIN
+}
---
+++ src/test/java/com/fifajwt/amigosjwt/AmigosjwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fifajwt.amigosjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmigosjwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
