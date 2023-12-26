5b86c65b4de5b9a48b34924f2dab42aab6c55e0e
Mohd Taiyyab <taiyyabmohammad11@gmail.com>
2023-12-26 21:31:45 +0530

    Fix: 403 Error

--- src/main/java/com/fifajwt/amigosjwt/demo/DemoController.java
+++ src/main/java/com/fifajwt/amigosjwt/demo/DemoController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@RequestMapping("/api/v1/demo")
+@RequestMapping("/api/v1/auth/demo")
 public class DemoController {
 
     Logger log = LoggerFactory.getLogger(DemoController.class);
